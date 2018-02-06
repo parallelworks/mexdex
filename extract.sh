77f6d2253f9b2b16ec857059e2253a107d74baf3
marmar-m <marmar@parallelworks.com>
2018-02-05 18:25:20 -0600

    fix extract.sh to run extract.py

--- extract.sh
+++ extract.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
-
+#
+# This script provides examples for running extract.py via pvpython. To get help on the arguments for
+# running extract.py, run: 
+#
+#   ParaviewPath/bin/pvpython  extract.py -h
+#
 # USAGE:
 # - CalculiX (exo file):
 #    ./extract.sh /opt/paraview530/bin solve.exo beadOnPlateKPI.json example_outputs outputs/metrics.csv 
 # - openFOAM:
-#    ./extract.sh /opt/paraview530/bin elbow-test/system/controlDict elbowKPI.json outputs/  outputs/metrics.csv 
+#    ./extract.sh /opt/paraview530/bin elbow-test/system/controlDict elbowKPI.json outputs/  outputs/metrics.csv 0
 
 paraviewPath=$1
 resultsFile=$2
 desiredMetricsFile=$3
 pvOutputDir=$4
 outputMetrics=$5
-caseNumber=$6
 
-if [ $# -eq 7 ]
+if [ $# -ge 6 ]
 then
-	isCellData=$7
+	caseNumber="--case_number $6"
 else
-	isCellData=""
+	caseNumber=""
 fi
 
-xvfb-run -a --server-args="-screen 0 1024x768x24" $paraviewPath/pvpython  --mesa-llvm   extract.py  $resultsFile $desiredMetricsFile  $pvOutputDir $outputMetrics $caseNumber $isCellData
+conver2cellData=""
+if [ $# -eq 7 ]
+then
+	if [ "$7" = true ] ; then
+		convert2cellData="--convert_to_cell_data"
+	fi
+fi
+
+xvfb-run -a --server-args="-screen 0 1024x768x24" $paraviewPath/pvpython  --mesa-llvm   extract.py  $resultsFile $desiredMetricsFile  $pvOutputDir $outputMetrics $caseNumber $convert2cellData 
 
 
