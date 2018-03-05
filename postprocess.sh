51c5e74eda8e72501aca2ff60583f56cc04ce8a1
marmar-m <marmar@parallelworks.com>
2018-03-05 17:24:01 -0600

    return all result files in postprocess.sh

--- postprocess.sh
+++ postprocess.sh
@@ -2,24 +2,25 @@
 
 outcsv=$1
 outhtml=$2
-rpath=$3
-caseslistFile=$4
-metrics_json=$5
+outzip=$3
+rpath=$4
+caseslistFile=$5
+metrics_json=$6
 
-if [ $# -lt 6 ]
+if [ $# -lt 7 ]
 then
 	mexdexDir="."
 else
-	mexdexDir=$6
+	mexdexDir=$7
 fi 
 
 # For cases were png directory and case directory roots are not specified in the kpi.json file:
 setRootDir=false
-if [ $# -eq 9 ]
+if [ $# -eq 10 ]
 then
-	pngOutDirRoot=$7
-	caseDirRoot=$8   
-	outputsList4DE=$9
+	pngOutDirRoot=$8
+	caseDirRoot=$9   
+	outputsList4DE=$10
 	setRootDir=true
 fi
 
@@ -42,7 +43,8 @@ fi
 # Generate the design explorer csv file:
 #
 if [ "$setRootDir" = true ] ; then
-	# If the root directories are provided as input to this script, set them when calling mexdex/writeDesignExplorerCsv.py 
+	# If the png and case root directories are provided as input to this script, set them when
+	# calling mexdex/writeDesignExplorerCsv.py 
 	# Works with both python2 and python3 
 	python   $mexdexDir/writeDesignExplorerCsv.py \
 		--casesList_paramValueDelimiter "=" \
@@ -64,4 +66,6 @@ fi
 baseurl="$DEbase/DesignExplorer/index.html?datafile=$basedir/$outcsv&colorby=$colorby"
 echo '<html style="overflow-y:hidden;background:white"><a style="font-family:sans-serif;z-index:1000;position:absolute;top:15px;right:0px;margin-right:20px;font-style:italic;font-size:10px" href="'$baseurl'" target="_blank">Open in New Window</a><iframe width="100%" height="100%" src="'$baseurl'" frameborder="0"></iframe></html>' > $outhtml
 
+zip tmp.zip results -r
+mv tmp.zip $outzip
 
